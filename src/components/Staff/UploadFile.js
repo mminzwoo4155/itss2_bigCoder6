cf3497843c55629bdbde83112228287a26515055
dutran917 <ductcp@gmail.com>
2023-01-07 12:06:36 +0700

    upload file pdf

---
+++ src/components/Staff/UploadFile.js
@@ -0,0 +1,43 @@
+import { Button, Col, Form, Input, notification, Row } from "antd";
+import React, { useState } from "react";
+import { storage } from "../../firebase";
+const UploadFile = ({ setFile }) => {
+  //   const [form] = Form.useForm();
+  //   const [file, setFile] = useState("");
+  //   const upload = () => {
+  //     if (file == null) return;
+
+  //     //   .on("state_changed", alert("success"), alert);
+  //   };
+  return (
+    // <Form layout="inline">
+    <Form.Item label="Upload file PDF">
+      <Input
+        type="file"
+        accept="application/pdf,application/vnd.ms-excel"
+        multiple={false}
+        onChange={(e) => {
+          // setFile(e.target.files[0]);
+          storage
+            .ref(`/file/${e.target.files[0].name}`)
+            .put(e.target.files[0])
+            .then((res) => {
+              notification.success({
+                message: "Tải file thành công",
+              });
+
+              res.ref.getDownloadURL().then((url) => setFile(url));
+              //   setFile(`gs://big-coder-6.appspot.com/${res.metadata.fullPath}`);
+            })
+            .catch(() => {
+              notification.error({
+                message: "Tải file thất bại",
+              });
+            });
+        }}
+      />
+    </Form.Item>
+  );
+};
+
+export default UploadFile;
